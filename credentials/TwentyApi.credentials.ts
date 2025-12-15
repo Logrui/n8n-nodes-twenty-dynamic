d09bfb2a7b346ab5299d24fc93eea3ad0d2a4730
SYHC <yhcsanction@gmail.com>
2025-12-15 18:26:37 -0500

    fix: hide schema cache in credentials

--- credentials/TwentyApi.credentials.ts
+++ credentials/TwentyApi.credentials.ts
@@ -25,11 +25,8 @@ export class TwentyApi implements ICredentialType {
 		{
 			displayName: 'Schema Cache',
 			name: 'schemaCache',
-			type: 'json',
+			type: 'hidden',
 			default: '',
-			displayOptions: {
-				show: {},
-			},
 			description: 'Stores the API schema to reduce API calls. Will be updated automatically.',
 		},
 	];
@@ -43,7 +40,7 @@ export class TwentyApi implements ICredentialType {
 		},
 	};
 
-		test: ICredentialTestRequest = {
+	test: ICredentialTestRequest = {
 		request: {
 			baseURL: '={{$credentials?.domain}}',
 			url: '/open-api/core',
